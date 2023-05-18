7c841c03c00370647d9c5a09aba2ed3a0bd186b7
Junior Fernando <junior_618@hotmail.com>
2023-05-18 10:18:40 -0300

    Criado Banco de Dados

--- ProjetoMvc/Program.cs
+++ ProjetoMvc/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProjetoMvc.Models;
 namespace ProjetoMvc
 {
     public class Program
@@ -5,6 +8,8 @@ namespace ProjetoMvc
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<ProjetoMvcContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ProjetoMvcContext") ?? throw new InvalidOperationException("Connection string 'ProjetoMvcContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
