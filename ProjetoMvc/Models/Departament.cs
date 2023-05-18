694b39c0e81d3a5c84627928a0fb782656bc53e4
Junior Fernando <junior_618@hotmail.com>
2023-05-17 22:42:44 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoMvc/Models/Departament.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetoMvc.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
